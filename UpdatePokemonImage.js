9627eabab53b9bbb17c2189e04713f12ad687c4b
duyk16 <duyk16@gmail.com>
2018-12-24 01:07:23 +0700

    Create project

---
+++ UpdatePokemonImage.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const request = require('request');
+for (let i = 1; i <= 151; i++) {
+  request('https://assets.pokemon.com/assets/cms2/img/pokedex/full/' + i + '.png')
+    .pipe(fs.createWriteStream('./img/pokemon/' + i + '.png'))
+}
